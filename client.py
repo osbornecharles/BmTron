0f7fcbd0d0ece4d688b546aaac55831d1f0d389a
mimichen226 <mchen6@nd.edu>
2017-05-06 16:35:48 -0400

    Added communication between client and server players

---
+++ client.py
@@ -0,0 +1,116 @@
+# Client player
+
+# Mimi Chen (mchen6)
+
+from twisted.internet.protocol import ClientFactory
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor
+from twisted.internet.defer import DeferredQueue
+from twisted.python import log
+import sys
+log.startLogging(sys.stdout)
+
+COMMAND_PORT = 40128
+DATA_PORT = 42128
+
+# ======================= CONNECTIONS =========================================
+class CommandConnection(Protocol):
+    '''Handles command connection between work.py and home.py'''
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        print("Command connection: created from host player to client player")
+    
+    def dataReceived(self,data):
+        '''Upon receiving command that home.py connected with client, create data connection'''
+        print("Command connection: client player received data from host player: {}".format(data))
+        if (data == "opened_dataport"):
+            print("Command connection: client player connecting to host player's data port")
+            # Create data connection
+            reactor.connectTCP("ash.campus.nd.edu", DATA_PORT, DataConnectionFactory(self.factory))
+
+class DataConnection(Protocol):
+    '''Handles data connection between home.py and work.py'''
+    def __init__(self, factory):
+        self.factory = factory
+        self.queue = DeferredQueue()
+
+    def connectionMade(self):
+        '''Upon establishing data connection, create service connection'''
+        print("Data connection: created data connection between client player and host player")
+
+    def dataReceived(self, data):
+        '''Queue up data received from home.py while service connection is being established'''
+        print("Data connection: received data from host player")
+        self.queue.put(data)
+
+    def startForward(self):
+        '''Add a callback to deferred object waiting in the queue'''
+        print("Data connection: start forwarding from client player to host player")
+        self.queue.get().addCallback(self.forwardData)
+    
+    def forwardData(self, data):
+        '''Write data from work.py to home.py by getting from queue and calling its callback function'''
+        print("Data connection: forwarding data from client player to host player")
+        self.queue.get().addCallback(self.forwardData)
+
+# ==================== CONNECTION FACTORY =====================================
+class CommandConnectionFactory(ClientFactory):
+    '''Generates command connection'''
+    def __init__(self):
+        self.command_connection = CommandConnection(self)
+
+    def buildProtocol(self, addr):
+        return self.command_connection
+
+    def startedConnecting(self, connector): 
+        print("Command connection factory: started connecting with connector: {}".format(connector))
+
+class DataConnectionFactory(ClientFactory):
+    '''Generates data connections'''
+    def __init__(self, factory):
+        self.command_connection = factory.command_connection
+        self.data_connection = DataConnection(self)
+
+    def buildProtocol(self, addr):
+        return self.data_connection 
+    
+    def startedConnecting(self, connector): 
+        print("Data connection factory: started connecting with connector: {}".format(connector))
+
+
+
+
+# ======================== REACTOR ============================================
+# Create command connection
+reactor.connectTCP("ash.campus.nd.edu", COMMAND_PORT, CommandConnectionFactory()) 
+reactor.run()
+
+
+'''
+class ServiceConnectionFactory(ClientFactory): 
+    def __init__(self, factory):
+        self.command_connection = factory.command_connection
+        self.data_connection = factory.data_connection
+        self.service_connection = ServiceConnection(self)   
+    
+    def buildProtocol(self, addr):
+        return self.service_connection
+class ServiceConnection(Protocol):
+    def __init__(self, factory): 
+        self.factory = factory
+        self.queue = DeferredQueue()
+
+    def connectionMade(self):
+        print "Service connection: created service connection from work.py to student01"
+        self.factory.data_connection.startForward()
+    
+    def dataReceived(self,data):
+        print "Service connection: work.py received data from student01"
+        self.factory.data_connection.transport.write(data)
+
+    def connectionLost(self, reason):
+        print "Service connection: connection with service lost - {}".format(reason)
+'''
+
--- server.py
+++ server.py
@@ -6,6 +6,82 @@ from twisted.internet.defer import DeferredQueue
 import sys
 log.startLogging(sys.stdout)
 
+COMMAND_PORT = 40128
+DATA_PORT = 42128
+
+
+# ======================= CONNECTIONS =========================================
+class CommandConnection(Protocol):
+    '''Handles command connection between home.py and work.py'''
+    def __init__(self, factory): 
+        self.factory = factory
+
+    def connectionMade(self):
+        print("Command connection: created between host and client players")
+        # Listen for client port
+        print("Command connection: beginning to listen on DATA_PORT {} for data".format(DATA_PORT))
+        reactor.listenTCP(DATA_PORT, DataConnectionFactory(self.factory)) 
+        # Tell client player that the host player is listening on data port
+        self.transport.write("opened_dataport".encode())
+
+class DataConnection(Protocol):
+    '''Handles data connection between home.py and work.py'''
+    def __init__(self, factory):
+        self.datafactory = factory
+
+    def connectionMade(self):
+        '''Upon establishing data connection, start forwarding what is in client connection's queue'''
+        print("Data conection: Created connection between host and client players")
+
+    def dataReceived(self, data):
+        '''Use client conection to forward data from work.py to client'''
+        print("Data connection: Received data from client player")
+
+
+# ======================= CONNECTION FACTORY ==================================
+class CommandConnectionFactory(ServerFactory):
+    '''Generates command connection'''
+    def __init__(self):
+        self.command_connection = CommandConnection(self)
+
+    def buildProtocol(self, addr):
+        return self.command_connection
+
+    def startedConnecting(self, connector):
+        print("Command connection factory: Started connecting with client player: {}".format(connector))
+
+    def clientConnectionLost(self, connector, reason):
+        print('Command connection factory: Lost connection.  Reason:', reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print('Command connection factory: Connection failed. Reason:', reason)
+
+
+class DataConnectionFactory(ServerFactory):
+    '''Generates data connection'''
+    def __init__(self, factory):
+        self.command_connection = factory.command_connection
+        self.data_connection = DataConnection(self)
+
+    def buildProtocol(self, addr):
+        return self.data_connection
+    def startedConnecting(self, connector):
+        print("Data connection factory: Started connecting with client player: {}".format(connector))
+
+    def clientConnectionLost(self, connector, reason):
+        print('Data connection factory: Lost connection.  Reason:', reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print('Data connection factory: Connection failed. Reason:', reason)
+
+
+# ================= MAIN EXECUTION =========================
+#if __name__ == "__main__":
+    # Establish connection with client player
+#    reactor.listenTCP(COMMAND_PORT, CommandConnectionFactory())
+#    reactor.run()
+
+'''
 
 class DataConnection(Protocol):
     def __init__(self):
@@ -36,4 +112,35 @@ class DataConnectionFactory(ServerFactory):
     def clientConnectionFailed(self, connector, reason):
         print('Connection failed. Reason:', reason)
 
+class ClientConnection(Protocol): 
+    def __init__(self, factory):
+        self.factory = factory
+        self.queue = DeferredQueue()
+
+    def connectionMade(self):
+        print("Client connection: created between client and home.py")
+        self.factory = DataConnectionFactory(self.factory)
+        reactor.listenTCP(DATA_PORT, self.factory)
+        self.factory.command_connection.sendCommand()
+
+    def dataReceived(self, data): 
+        print("Client connection: received data from client")#.format(data)
+        self.queue.put(data)
+
+    def startForward(self):
+        print("Client connection: start forwarding data to client")
+        # Add a callback to the deferred object waiting in the queue
+        self.queue.get().addCallback(self.forwardData)
+    
+    def forwardData(self, data):
+        print("Client connection: forwarding data to client")
+        # Forward data by sending the data to work.py
+        self.factory.data_connection.transport.write(data)
+        # Since there may be multiple things on queue, must add itself back onto the 
+            # next deferred object
+        self.queue.get().addCallback(self.forwardData)
+
+    def connectionLost(self, reason):
+        print("Client connection: connection with client lost - {}".format(reason))
+'''
 
