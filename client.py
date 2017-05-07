87ed2a57ab49644412556eaabcb907343c4ddf62
mimichen226 <mchen6@nd.edu>
2017-05-07 12:28:02 -0400

    Data transfer

--- client.py
+++ client.py
@@ -13,45 +13,75 @@ DATA_PORT = 42128
 
 # ======================= CONNECTIONS =========================================
 class CommandConnection(Protocol):
-    '''Handles command connection between work.py and home.py'''
+    '''Handles command connection between client and host players'''
     def __init__(self, factory):
-        self.factory = factory
+        self.factory = factory 
+        self.sentStart = False
+        self.receivedStart = False
 
     def connectionMade(self):
         print("Command connection: created from host player to client player")
+
+    def sendStart(self): 
+        '''Send to host player that client player has pressed "start"'''
+        print("Command connection: sent start to host")
+        self.sentStart = True
+        self.transport.write("Start pressed".encode())
     
     def dataReceived(self,data):
-        '''Upon receiving command that home.py connected with client, create data connection'''
+        '''Handle received data from host player'''
         print("Command connection: client player received data from host player: {}".format(data))
+        # Host player is listening on its data port
         if (data.decode() == "opened_dataport"):
             print("Command connection: client player connecting to host player's data port")
             # Create data connection
             reactor.connectTCP("ash.campus.nd.edu", DATA_PORT, DataConnectionFactory(self.factory))
 
+        # Host player clicked "start"
+        elif (data.decode() == "Start pressed"):
+            print("Command connection: host player pressed start")
+            self.receivedStart = True
+
+    def start(self):
+        '''Return true only if both the host and the client players clicked "start"'''
+        return self.sentStart and self.receivedStart
+
+
 class DataConnection(Protocol):
-    '''Handles data connection between home.py and work.py'''
+    '''Handles data connection between host and client players'''
     def __init__(self, factory):
         self.factory = factory
-        self.queue = DeferredQueue()
+        #self.queue = DeferredQueue()    # Queue to hold all data 
+        self.queue = Queue()
 
     def connectionMade(self):
         '''Upon establishing data connection, create service connection'''
         print("Data connection: created data connection between client player and host player")
 
     def dataReceived(self, data):
-        '''Queue up data received from home.py while service connection is being established'''
-        print("Data connection: received data from host player")
-        self.queue.put(data)
+        '''Upon receiving data as a string, decode it and return as an array'''
+        self.queue.put(data.decode())
+        #data = rawdata.decode().split() # array 
 
-    def startForward(self):
-        '''Add a callback to deferred object waiting in the queue'''
-        print("Data connection: start forwarding from client player to host player")
-        self.queue.get().addCallback(self.forwardData)
-    
-    def forwardData(self, data):
-        '''Write data from work.py to home.py by getting from queue and calling its callback function'''
-        print("Data connection: forwarding data from client player to host player")
-        self.queue.get().addCallback(self.forwardData)
+    def returnData(self):
+        myQueue = self.queue
+        self.queue = Queue() # new queue for next round of data
+        return myQueue
+
+    def sendSpeed(self, speed): 
+        self.transport.write(" ".join("speed", str(speed)).encode())
+
+    def sendDirection(self, direction):
+        self.transport.write(" ".join("direction",str(direction)).encode())
+
+    def sendCollision(self):
+        self.transport.write("dead".encode())
+
+    def sendSizeChange(self, size):
+        self.transport.write((" ".join("size", size)).encode())
+
+    #def sendRetrievedItem(self, item): 
+    #    self.transport.write(" ".join("item",  item).encode())
 
 # ==================== CONNECTION FACTORY =====================================
 class CommandConnectionFactory(ClientFactory):
@@ -76,31 +106,4 @@ class DataConnectionFactory(ClientFactory):
     
     def startedConnecting(self, connector): 
         print("Data connection factory: started connecting with connector: {}".format(connector))
-
-
-'''
-class ServiceConnectionFactory(ClientFactory): 
-    def __init__(self, factory):
-        self.command_connection = factory.command_connection
-        self.data_connection = factory.data_connection
-        self.service_connection = ServiceConnection(self)   
-    
-    def buildProtocol(self, addr):
-        return self.service_connection
-class ServiceConnection(Protocol):
-    def __init__(self, factory): 
-        self.factory = factory
-        self.queue = DeferredQueue()
-
-    def connectionMade(self):
-        print "Service connection: created service connection from work.py to student01"
-        self.factory.data_connection.startForward()
-    
-    def dataReceived(self,data):
-        print "Service connection: work.py received data from student01"
-        self.factory.data_connection.transport.write(data)
-
-    def connectionLost(self, reason):
-        print "Service connection: connection with service lost - {}".format(reason)
-'''
 
--- p2.py
+++ p2.py
@@ -6,6 +6,111 @@ from client import *
 
 media_files = "./mediafiles/"
 COMMAND_PORT = 40128
+CELL_SIZE = 20 
+
+# DIRECTION
+    # 0 is north
+    # 1 is east
+    # 2 is south
+    # 3 is west
+
+class Player2(pygame.sprite.Sprite):
+    def __init__(self, gamespace):
+        self.gs = gamespace
+        self.name = "doge"
+        self.image = pygame.image.load(media_files + "doge.png")
+        self.rect = self.image.get_rect()
+        self.x = self.gs.width * 3 /4
+        self.y = self.gs.height / 2
+        self.direction = 3 # heads west
+        self.rect.move_ip(self.x, self.y) # start on right side
+        self.copy = self.image
+
+        self.gs.board.putOnBoard(self, self.x/CELL_SIZE, self.y/CELL_SIZE)
+
+    def tick(self):
+        pass
+ 
+    def move(self, key_event):
+        pass
+
+class Player1(pygame.sprite.Sprite):
+    def __init__(self, gamespace):
+        self.gs = gamespace
+        self.name = "gabe"
+        self.image = pygame.image.load(media_files + "gabe.png")
+        self.rect = self.image.get_rect()
+        self.x = self.gs.width/4 
+        self.y = self.gs.height/2
+        self.direction = 1 # Heads east
+        self.rect.move_ip(self.x, self.y) # start on left side
+        self.copy = self.image
+        
+        self.gs.board.putOnBoard(self, self.x/CELL_SIZE, self.y/CELL_SIZE)
+
+    def tick(self):
+        '''Send location to other player'''
+        # Retrieve data from host player about Player 1
+        dataQueue = self.gs.data_connection.returnData()
+        while (not dataQueue.empty()):
+            data = dataQueue.get().split()
+
+            # Host player died
+            if (data[0] == "dead"):
+                print("Client player wins!")
+
+            # Host player changed size
+            elif (data[0] == "size"):
+                size = int(data[1])
+
+            # Host player got item
+            #elif (data[0] == "item"):
+            #    item = data[1]
+
+            # Host player changed direction
+            elif (data[0] == "direction"):
+                direction = int(data[1])
+
+            # Host player changed speed
+            elif (data[0] == "speed"):
+                speed = int(data[1])
+
+        # UPDATE Player 1 (host player) with received data
+
+    def move(self):
+        pass
+
+class Mud(pygame.sprite.Sprite):
+    def __init__(self, gamespace): 
+        self.gs = gamespace
+        self.name = "mud"
+        self.image = pygame.image.load(media_files + "Poop_Emoji.png")
+
+class Board():
+    def __init__(self, gamespace):
+        self.gs = gamespace
+        self.board = []
+        for y in range(int(self.gs.height / CELL_SIZE)):
+            row = []
+            for x in range(int(self.gs.width / CELL_SIZE)):
+                row.append(0)
+            self.board.append(row)
+
+    def putOnBoard(self, thing, x, y):
+        print("{} at x = {}, y = {}".format(thing.name, x,y))
+        if (thing.name == "gabe"):
+            self.board[y][x] = 1
+        elif (thing.name == "doge"):
+            self.board[y][x] = 2
+
+    def deleteFromBoard(self, x, y):
+        self.board[y][x] = 0
+
+    def printBoard(self):
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                print("| {} ".format(board[y][x]), end = '')
+            print ("\n----------------------------")
 
 class GameSpace:
     def titleScene(self, factory): 
@@ -76,9 +181,18 @@ class GameSpace:
 
     def gameScene(self):
         # Set up game objects
+        self.board = Board(self)
+        self.p1 = Player1(self)
+        self.p2 = Player2(self)
+
+        # Draw players onto screen
+        self.screen.blit(self.p1.image, self.p1.rect)
+        self.screen.blit(self.p2.image, self.p2.rect)
+
+        self.board.printBoard()
 
         # List to hold all other game objects
-        self.all_objects = []
+        #self.all_objects = [self.p1, self.p2]
 
         # Start game loop
         self.loop = LoopingCall(self.gameloop)
@@ -87,8 +201,14 @@ class GameSpace:
 
     def gameloop(self):
         print("In game loop")
-        pass
-                      
+        #for event in pygame.event.get():
+            #if event.type == pygame.KEYDOWN:
+        
+        for obj in self.all_objects:
+            obj.tick()
+
+        pygame.display.flip()
+                
 
 if __name__ == "__main__":
     # Instantiate CommandConnectionFactory 
