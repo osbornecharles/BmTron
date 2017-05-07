becef286fa5ec87ea6c669b1b576d883a7331359
mimichen226 <mchen6@nd.edu>
2017-05-07 10:12:41 -0400

    Added game loop

--- p2.py
+++ p2.py
@@ -4,25 +4,29 @@ from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
 from client import *
 
-media_file_path = "./mediafiles/"
-title = 1
+media_files = "./mediafiles/"
 COMMAND_PORT = 40128
 
 class GameSpace:
-    def main(self): 
-        # Step 1: Initialize the game space
+    def titleScene(self, factory): 
+        '''Title scene with start button'''
+        # Save factory
+        self.factory = factory
+
+        # Initialize the game space
         pygame.init()
         self.size = self.width, self.height = 640, 420
         self.black = 0,0,0
         self.screen = pygame.display.set_mode(self.size)
         pygame.key.set_repeat(10, 30)
 
+        # Menu's game title text
         font = pygame.font.Font(None, 20)
         self.startText = font.render('Start', 0, (30, 120, 50))
         bigfont = pygame.font.Font(None, 40)
         self.title = bigfont.render('Game', 0, (250, 250, 250))
 
-
+        # Menu's start button 
         self.startButton = pygame.Rect(0, 0, 100, 40)
         self.startButton.topleft = (self.width/2 - self.startButton.width/2, self.height/2)
         self.textRect = self.startText.get_rect()
@@ -30,47 +34,71 @@ class GameSpace:
         self.titleRect = self.title.get_rect()
         self.titleRect.topleft = (self.width/2 - self.titleRect.width/2, 10)
         pygame.draw.rect(self.screen, (30, 120, 50), self.startButton, 1)
+
+        # Draw title and startText to screen
         self.screen.blit(self.startText, self.textRect)
         self.screen.blit(self.title, self.titleRect)
         pygame.display.flip()
 
+        # Start game loop
+        self.loop = LoopingCall(self.titleloop)
+        self.loop.start(1/60)
 
-        # Step 2: Initialize the game objects and place into an array
-        #self.clock = pygame.time.Clock()
-
-        self.all_objects = []
-
-    def titleloop(self, factory):
-        global title
-        if title:
+    def titleloop(self):
+        # Stay at title scene if both players have not selected start
+        if (not self.factory.command_connection.start()):
             for event in pygame.event.get():
+                # Close pygame
                 if event.type == pygame.QUIT:
-                    sys.exit()
+                    reactor.stop()
+                # Key detection
                 elif event.type == pygame.KEYDOWN:
-                    # Escape key
+                    # Escape key to end pygame
                     if (event.key == pygame.K_ESCAPE):
-                        sys.exit()
+                        reactor.stop()
+                # Mouse detection
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
+                    # User clicked start
                     if self.startButton.collidepoint(mouse_pos):
-                        title = 0
-                        factory.command_connection.transport.write('Start pressed')
-
+                        self.factory.command_connection.sendStart()
             self.screen.blit(self.startText, self.textRect)
             self.screen.blit(self.title, self.titleRect)
             pygame.display.flip()
+        # Transition to game scene
         else:
             print("hello")
             self.screen.fill(self.black)
             pygame.display.flip()
+            # Stop title loop and go to game scene
+            self.loop.stop()
+            self.gameScene()
+
+    def gameScene(self):
+        # Set up game objects
+
+        # List to hold all other game objects
+        self.all_objects = []
+
+        # Start game loop
+        self.loop = LoopingCall(self.gameloop)
+        self.loop.start(1/60)
+        print("In game scene: started game loop")
+
+    def gameloop(self):
+        print("In game loop")
+        pass
                       
 
 if __name__ == "__main__":
-    gs = GameSpace()
-    gs.main()
+    # Instantiate CommandConnectionFactory 
     factory = CommandConnectionFactory()
-    reactor.connectTCP('localhost', COMMAND_PORT, factory)
+
+    # Instantiate gamspace and call its menu function
+    gs = GameSpace()
+    gs.titleScene(factory)
+
+    # Connect to host's command port and start the reactor for event driven system 
+    reactor.connectTCP("ash.campus.nd.edu", COMMAND_PORT, factory)
     reactor.run()
-    tl = LoopingCall(gs.titleloop, [factory])
-    tl.start(1/60)
 
